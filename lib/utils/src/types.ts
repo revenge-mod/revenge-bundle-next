bb29ea3902026cab6ffc13a38d2874cc4390c5de
PalmDevs <git@palmdevs.me>
2025-11-12 17:24:28 +0700

    feat(lib/utils/types): add `AnyFunction`

--- lib/utils/src/types.ts
+++ lib/utils/src/types.ts
@@ -2,6 +2,7 @@ export type Nullish = null | undefined
 export type If<T, Then, Else> = T extends true ? Then : Else
 export type Not<T extends boolean> = T extends true ? false : true
 export type AnyObject = Record<any, any>
+export type AnyFunction = (...args: any[]) => any
 export type LogicalOr<T1, T2> = T1 extends true
     ? true
     : T2 extends true
