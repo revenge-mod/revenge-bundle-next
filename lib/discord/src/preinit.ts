c1a7ddbd44c7a0545499ce1446ac1caca21cf1ba
PalmDevs <git@palmdevs.me>
2026-01-24 18:01:48 +0700

    fix(lib/discord): correct AppStartPerformance filter

--- lib/discord/src/preinit.ts
+++ lib/discord/src/preinit.ts
@@ -1,17 +1,27 @@
 import { lookupModule } from '@revenge-mod/modules/finders'
 import {
     withDependencies,
+    withName,
     withProps,
 } from '@revenge-mod/modules/finders/filters'
 import type { Metro } from '@revenge-mod/modules/types'
 import type { DiscordModules } from './types'
 
-const { relative } = withDependencies
+const [, _asyncToGeneratorModuleId] = lookupModule(
+    withName('_asyncToGenerator'),
+)
+const [, _classCallCheckModuleId] = lookupModule(withName('_classCallCheck'))
+const [, _createClassModuleId] = lookupModule(withName('_createClass'))
 
 // ../discord_common/js/packages/app-start-performance/AppStartPerformance.tsx
 export const [AppStartPerformance] = lookupModule(
     withProps<DiscordModules.AppStartPerformance>('markAndLog').and(
-        withDependencies([relative(-1), null, null, null]),
+        withDependencies([
+            _asyncToGeneratorModuleId,
+            _classCallCheckModuleId,
+            _createClassModuleId,
+            2,
+        ]),
     ),
 ) as [DiscordModules.AppStartPerformance, Metro.ModuleID]
 
