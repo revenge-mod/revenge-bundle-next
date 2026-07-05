ddb02d2887744614c433673c83a58b99a0612ab3
PalmDevs <git@palmdevs.me>
2026-07-05 21:07:00 +0700

    fix(lib/externals/shopify): update filters for 337206+

--- lib/externals/src/shopify.ts
+++ lib/externals/src/shopify.ts
@@ -14,21 +14,36 @@ import { proxify } from '@revenge-mod/utils/proxy'
 export let FlashList: typeof import('@shopify/flash-list') = proxify(
     () => {
         const [module] = lookupModule(
-            withProps<typeof FlashList>('FlashList').and(
-                // Dependencies. One in brackets are dynamic or late initialized.
-                // [React, RN, JSXRuntime, (FlashListExports), (Reanimated), (RNBottomSheet), ImportTracker, (BottomSheetFlashList)]
-                withDependencies([
-                    ReactModuleId,
-                    ReactNativeModuleId,
-                    ReactJSXRuntimeModuleId,
-                    null,
-                    null,
-                    null,
-                    // TODO: Decouple?
-                    ImportTrackerModuleId,
-                    null,
-                ]),
-            ),
+            withProps<typeof FlashList>('FlashList')
+                .and(
+                    // [React, JSXRuntime, (Platform), (FlashListExports), (Reanimated), (RNBottomSheet), (BottomSheet), ImportTracker]
+                    withDependencies([
+                        ReactModuleId,
+                        ReactJSXRuntimeModuleId,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        // TODO: Decouple?
+                        ImportTrackerModuleId,
+                    ]).or(
+                        // TODO: Remove when stable > 337206
+                        // [React, RN, JSXRuntime, (FlashListExports), (Reanimated), (RNBottomSheet), ImportTracker, (BottomSheetFlashList)]
+                        withDependencies([
+                            ReactModuleId,
+                            ReactNativeModuleId,
+                            ReactJSXRuntimeModuleId,
+                            null,
+                            null,
+                            null,
+                            // TODO: Decouple?
+                            ImportTrackerModuleId,
+                            null,
+                        ]),
+                    ),
+                )
+                .keyAs('revenge.externals.Shopify.FlashList'),
         )
 
         if (module) return (FlashList = module)
