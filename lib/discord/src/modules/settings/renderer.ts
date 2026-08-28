3d532d90280a3cc644ef92e4e27d56599ea70061
PalmDevs <git@palmdevs.me>
2026-08-28 19:12:03 +0700

    fix: update filters for 344205+

--- lib/discord/src/modules/settings/renderer.ts
+++ lib/discord/src/modules/settings/renderer.ts
@@ -1,5 +1,6 @@
 import { lookupModule } from '@revenge-mod/modules/finders'
 import {
+    anyOf,
     withDependencies,
     withProps,
 } from '@revenge-mod/modules/finders/filters'
@@ -9,9 +10,10 @@ import {
     ReactNativeModuleId,
 } from '@revenge-mod/react'
 import { proxify } from '@revenge-mod/utils/proxy'
+import { ImportTrackerModuleId } from '../../patches/import-tracker'
 import type { DiscordModules } from '../../types'
 
-const { loose, relative } = withDependencies
+const { loose, relative, last } = withDependencies
 
 export type SettingListRenderer =
     DiscordModules.Modules.Settings.SettingListRenderer
@@ -22,15 +24,25 @@ export let SettingListRenderer: SettingListRenderer = proxify(
         const [module] = lookupModule(
             withProps<SettingListRenderer>('SettingsList')
                 .and(
-                    withDependencies(
-                        loose([
-                            ReactModuleId,
-                            ReactNativeModuleId,
-                            relative(1),
-                            relative(2),
-                            null,
-                            ReactJSXRuntimeModuleId,
-                        ]),
+                    anyOf(
+                        withDependencies(
+                            loose([
+                                ReactModuleId,
+                                ReactNativeModuleId,
+                                relative(1),
+                            ]),
+                        ).and(withDependencies(last([ImportTrackerModuleId]))),
+                        // TODO: Remove this once stable > 344205
+                        withDependencies(
+                            loose([
+                                ReactModuleId,
+                                ReactNativeModuleId,
+                                relative(1),
+                                relative(2),
+                                null,
+                                ReactJSXRuntimeModuleId,
+                            ]),
+                        ),
                     ),
                 )
                 .keyAs(
